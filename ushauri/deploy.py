0011073917c4420ab5f8bfc485deef0325b4dde1
chepkoy <allankiplangat22@gmail.com>
2017-11-09 01:12:31 +0300

    Rectified the and fixed the production environmrnt settings

--- ushauri/deploy.py
+++ ushauri/deploy.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ushauri.deploy")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ushauri.deploy_settings")
 
 application = get_wsgi_application()
