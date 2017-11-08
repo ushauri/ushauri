88c7c529c5f95df9533e4ead45c1a879f66ecb70
chepkoy <allankiplangat22@gmail.com>
2017-11-09 01:40:45 +0300

    Added postgress Database to settings

--- ushauri/deploy_settings/__init__.py
+++ ushauri/deploy_settings/__init__.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from ushauri.settings import *
 
 DEBUG = False
@@ -7,3 +9,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '.herokuapp.com',
 ]
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
