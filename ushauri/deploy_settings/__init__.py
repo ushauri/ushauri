c74411cdbff9e5b4ae7301f08933c96f56770c3c
chepkoy <allankiplangat22@gmail.com>
2017-11-09 13:55:26 +0300

    Add whitenoise to project

--- ushauri/deploy_settings/__init__.py
+++ ushauri/deploy_settings/__init__.py
@@ -14,3 +14,6 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+
