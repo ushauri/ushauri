ab2c115da9a44885c35fa0602724acb9c74feb44
chepkoy <allankiplangat22@gmail.com>
2017-11-09 20:12:30 +0300

    Removed account url

--- ushauri/urls.py
+++ ushauri/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 from . import views
@@ -22,5 +22,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.landing),
     url(r'^admin/', admin.site.urls),
-    url(r"^accounts/", include("account.urls")),
 ]
